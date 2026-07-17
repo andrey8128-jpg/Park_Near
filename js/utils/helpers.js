0f795ba3a17eb463a9424ed90d23ceabc53a2725
andrey8128-jpg <andrey.8128@mail.ru>
2026-07-17 22:21:24 +0400

    helpers.js

---
+++ js/utils/helpers.js
@@ -0,0 +1,7 @@
+import { regionsData } from './constants.js';
+
+export function formatDateTime(timestamp) { /* ... */ }
+export function getDistanceInMeters(lat1, lng1, lat2, lng2) { /* ... */ }
+export function parseAddress(fullAddress) { /* ... использует regionsData ... */ }
+export function getOccupancyColor(occupied, total) { /* ... */ }
+// остальные вспомогательные функции (checkPolygonSize и т.д.) тоже можно сюда
