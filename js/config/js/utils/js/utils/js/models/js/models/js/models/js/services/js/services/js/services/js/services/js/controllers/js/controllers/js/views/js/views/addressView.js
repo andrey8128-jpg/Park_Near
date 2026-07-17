7cfbe461f1c4396947dc227443218a6c66eafdb1
andrey8128-jpg <andrey.8128@mail.ru>
2026-07-17 21:59:37 +0400

    addressView.js

---
+++ js/config/js/utils/js/utils/js/models/js/models/js/models/js/services/js/services/js/services/js/services/js/controllers/js/controllers/js/views/js/views/addressView.js
@@ -0,0 +1,11 @@
+export function renderAddressList(addresses) {
+    // формирует список адресов с метками и кнопкой "Найти парковку"
+}
+
+export function renderAddressForm() {
+    // форма добавления адреса (с переключателем ручной/карта)
+}
+
+export function renderFavorites(content) {
+    // загружает адреса и избранные парковки
+}
