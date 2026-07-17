0aa8a9d59e31149a20d0f711155b9af937229f18
andrey8128-jpg <andrey.8128@mail.ru>
2026-07-17 22:29:01 +0400

    parkingView.js

---
+++ js/views/parkingView.js
@@ -0,0 +1,8 @@
+export function renderEditPanel(data, isFavorite) {
+    // генерирует HTML для редактирования парковки
+    // возвращает строку
+}
+
+export function renderParkingSheet(id, data) {
+    // отображает нижний лист с информацией и кнопкой "Маршрут"
+}
