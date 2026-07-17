6048a6e98d478459f87b4ea89837e7df37d1aacc
andrey8128-jpg <andrey.8128@mail.ru>
2026-07-17 22:50:37 +0400

    toggle.js

--- js/components/toggle.js
+++ js/components/toggle.js
@@ -1,3 +1,13 @@
 export function renderToggle(checked, onToggle) {
-    // возвращает HTML переключателя (switch)
+    const label = document.createElement('label');
+    label.className = 'theme-switch';
+    label.innerHTML = `
+        <input type="checkbox" ${checked ? 'checked' : ''}>
+        <span class="theme-slider"></span>
+    `;
+    const input = label.querySelector('input');
+    input.addEventListener('change', () => {
+        if (onToggle) onToggle(input.checked);
+    });
+    return label;
 }
