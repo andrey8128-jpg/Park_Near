de5adc2580eb8c9115e926b7e1ee039e5b9f8362
andrey8128-jpg <andrey.8128@mail.ru>
2026-07-17 21:50:18 +0400

    constants.js

---
+++ js/config/js/utils/constants.js
@@ -0,0 +1,3 @@
+export const carBrands = { /* ... полный объект из монолита ... */ };
+export const carColors = { /* ... полный объект ... */ };
+export const regionsData = { /* ... полный объект ... */ };
